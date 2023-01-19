c8e173639398eb87606543bd838c00d0eac4135a
ChetanXpro <chetanbaliyan10@gmail.com>
2023-01-19 18:42:53 +0530

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# falaina-documents
---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <div className="bg-red-500">hello</div>;
+}
+
+export default App;
